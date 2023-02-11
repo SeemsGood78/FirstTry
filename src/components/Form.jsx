fd87c805a709a9b6db8071cebe0e6f3092ddbabd
Pablo <seemsgood783@gmail.com>
2023-02-11 20:59:03 +0200

    validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,34 +2,62 @@ import React, { useState } from "react"
 
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 
-const disableLatters = (e, setName) => {
+const disableLatters = (e, cap) => {
     const result = e.target.value.replace(/[^a-z]/gi, '');
-    setName(result);
+    cap(result);
 };
+
 
 const Form = () => {
     const [name, setName] = useState('');
     const [surname, setSurname] = useState('');
     const [nameError, setNameError] = useState('');
+    const [surnameError, setSurnameError] = useState('');
     const [nameTouched, setNameTouched] = useState(false);
     const [surnameTouched, setSurnameTouched] = useState(false);
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [emailTouched, setEmailTouched] = useState(false);
+    const [phone, setPhone] = useState('');
+    const [phoneError, setPhoneError] = useState('');
+    const [phoneTouched, setPhoneTouched] = useState(false);
+    const [password, setPassword] = useState('');
 
-    const onChangeHandler = (e, setName, setErrors) => {
-        const { name, value } = e.target;
-        disableLatters(e, setName)
-
-        if (value.length === 0) {
-            setNameError(`${name} can't be empty`)
+    const emailCheck = (e) =>{
+        setEmail(e.target.value)
+        if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email) === false){
+            setEmailError('Invalid email')
         }
-        else if (value.length > 8) {
-            setNameError(`${name} field is too long`)
-        }
-        else if (value.length < 2) {
-            setNameError(`${name} field is too short`)
-        }
-        else setNameError(undefined)
+        else setEmailError(undefined)
     }
 
+    const phoneCheck = (e) =>{
+        setPhone(e.target.value)
+        if (/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(phone) === false){
+            setPhoneError('Invalid phone number')
+        }
+        else setPhoneError(undefined)
+    }
+
+    const clearPassword = () => {
+        setPassword('')
+    }
+
+    const onChangeHandler = (e, cap, Errors) => {
+        const { name, value } = e.target;
+        disableLatters(e, cap)
+
+        if (value.length === 0) {
+            Errors(`${name} can't be empty`)
+        }
+        else if (value.length > 8) {
+            Errors(`${name} field is too long`)
+        }
+        else if (value.length < 2) {
+            Errors(`${name} field is too short`)
+        }
+        else Errors(undefined)
+    }
 
     return (
         <>
@@ -40,27 +68,53 @@ const Form = () => {
                         type="text"
                         placeholder="Name"
                         value={capitalize(name)}
-                        name='name'
-                        onChange={e => onChangeHandler(e, setName)}
+                        name='Name'
+                        onChange={e => onChangeHandler(e ,setName ,setNameError)}
                         onBlur={() => setNameTouched(true)}
                     />
                     <input
                         type="text"
                         placeholder="Surname"
+                        name='Surname'
                         value={capitalize(surname)}
-                        // Сделать так что бы не передавали 'Surname'
-                        onChange={e => onChangeHandler(e, 'Surname', setSurname)}
+                        onChange={e => onChangeHandler(e,setSurname ,setSurnameError)}
                         onBlur={() => setSurnameTouched(true)}
                     />
                 </label>
-                {surnameTouched && nameError && <div className="error">{nameError}</div>}
+                {surnameTouched && surnameError && <div className="error">{surnameError}</div>}
                 {nameTouched && nameError && <div className="error">{nameError}</div>}
                 <label>
-                    <input type="text" name="email" placeholder="Email" />
-                    <input type="text" placeholder="Phone" maxLength={13} />
+                    <input 
+                    type="text" 
+                    name="email"
+                    placeholder="Email" 
+                    value={email}
+                    onChange={e => emailCheck(e)}
+                    onBlur={() => setEmailTouched(true)}
+                    />
+                    <input 
+                    type="text" 
+                    placeholder="Phone"
+                    name="phone"
+                    value={phone}
+                    onChange={e => phoneCheck(e)}
+                    onBlur={() => setPhoneTouched(true)}
+                    />
                 </label>
+                {emailTouched && emailError && <div className="error">{emailError}</div>}
+                {phoneTouched && phoneError && <div className="error">{phoneError}</div>}
                 <label>
-                    <input type="text" name='password' placeholder="Password" />
+                    <input
+                    type="password"
+                    name='password'
+                    placeholder="Password" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                    <div 
+                    className={password? 'reset' : 'display-none'}
+                    onClick={()=> clearPassword()}
+                    >12</div>
                 </label>
                 <label>
                     <span>
